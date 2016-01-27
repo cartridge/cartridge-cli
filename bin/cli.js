86fd65dddd74cc679ca1dea3fcdbb36c3ed7e723
tristanashley <tristan.ashley@codecomputerlove.com>
2016-01-27 15:32:40 +0000

    feat: Add in base CLI interface

--- bin/cli.js
+++ bin/cli.js
@@ -1,3 +1,17 @@
 #!/usr/bin/env node
 
-console.log('hits cli.js file');
+var program = require('commander');
+var prompt = require('inquirer');
+var path = require('path');
+
+var pkg = require(path.resolve(__dirname, '..', 'package.json'));
+
+program.version(pkg.version)
+
+program
+    .command('new')
+    .action(function() {
+        console.log('hits new command');
+    });
+
+program.parse(process.argv);
