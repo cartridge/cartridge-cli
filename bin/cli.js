f17aea4fed7652378310b53630191e8a48461716
tristanashley <tristan.ashley@codecomputerlove.com>
2016-01-27 15:44:51 +0000

    feat: Add in required argument name for new command

--- bin/cli.js
+++ bin/cli.js
@@ -9,9 +9,11 @@ var pkg = require(path.resolve(__dirname, '..', 'package.json'));
 program.version(pkg.version)
 
 program
-    .command('new')
-    .action(function() {
+    .command('new <name>')
+    .description('Create a new project with the provided name')
+    .action(function(name) {
         console.log('hits new command');
+        console.log('project name is ->', name);
     });
 
 program.parse(process.argv);
