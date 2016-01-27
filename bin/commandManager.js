e4b044afd1c9970680348b81348879ec490dbcbe
tristanashley <tristan.ashley@codecomputerlove.com>
2016-01-27 17:09:46 +0000

    feat: Add command manager to contain all command functionality

---
+++ bin/commandManager.js
@@ -0,0 +1,25 @@
+var exports = module.exports;
+var chalk = require('chalk');
+var inquirer = require('inquirer');
+var fs = require('fs-extra');
+var path = require('path');
+
+var libDir = path.resolve(__dirname, '..', 'lib'); 
+
+exports.newCallback = function(name) {
+    console.log('Creating a new project: %s', chalk.underline(name));
+
+    inquirer.prompt([{
+        type: "confirm",
+        name: "needTravis",
+        message: "Copying over files to current directory. Press enter to confirm",
+        default: true 
+    }], function(answers) {
+        console.log('copying over files...');
+
+        fs.copy(libDir, process.cwd(), function (err) {
+          if (err) return console.error(err)
+          console.log("success! - files copied");
+        })
+    })
+}
--- bin/programBuiider.js
+++ bin/programBuiider.js
@@ -1,13 +1,9 @@
-var program = require('commander');
-var inquirer = require('inquirer');
-var path = require('path');
-var chalk = require('chalk');
-var fs = require('fs-extra');
-
-var pkg = require(path.resolve(__dirname, '..', 'package.json'));
-var libDir = path.resolve(__dirname, '..', 'lib'); 
-
 var exports = module.exports;
+var program = require('commander');
+var path = require('path');
+
+var commandManager = require('./commandManager');
+var pkg = require(path.resolve(__dirname, '..', 'package.json'));
 
 exports.init = function() {
     setProgramVersion();
@@ -19,23 +15,7 @@ function setNewCommand() {
     program
         .command('new <name>')
         .description('Create a new project with the provided name')
-        .action(function(name) {
-            console.log('Creating a new project: %s', chalk.underline(name));
-
-            inquirer.prompt([{
-                type: "confirm",
-                name: "needTravis",
-                message: "Copying over files to current directory. Press enter to confirm",
-                default: true 
-            }], function(answers) {
-                console.log('copying over files...');
-
-                fs.copy(libDir, process.cwd(), function (err) {
-                  if (err) return console.error(err)
-                  console.log("success! - files copied");
-                })
-            })
-        });
+        .action(commandManager.newCallback);
 }
 
 function initProgram() {
