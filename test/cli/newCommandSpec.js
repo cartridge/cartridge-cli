45ca4dbb616eabe04f28e8764eda8d63fddb86a3
tristanashley <tristan.ashley@codecomputerlove.com>
2016-02-01 12:31:03 +0000

    tests: Add tests to check that expected files are excluded for a Dot NET build

--- test/cli/newCommandSpec.js
+++ test/cli/newCommandSpec.js
@@ -1,12 +1,9 @@
 var os = require('os');
 var fs = require('fs-extra');
 var path = require('path');
+var proxyquire = require('proxyquire');
 var chai = require('chai');
 
-chai.use(require('chai-fs'));
-chai.should();
-
-var proxyquire = require('proxyquire');
 var DotNetAnswersFixture = require('./fixtures/DotNetNoDescription');
 var newCommand = proxyquire('../../bin/commands/new', {
     inquirer: {
@@ -22,29 +19,44 @@ var newCommandInstance = newCommand(appDir);
 var date = new Date();
 var timestamp = [date.getDate(), date.getDay(), date.getFullYear(), date.getHours(), date.getMinutes(), date.getMilliseconds()].join('');
 
-var TEMP_DIR = path.join(os.tmpdir(), 'slate-test-' + timestamp);
+var TEST_TEMP_DIR = path.join(os.tmpdir(), 'slate-' + timestamp);
+
+chai.use(require('chai-fs'));
+chai.should();
 
 describe('As a user of the CLI', function() {
 
     before(function(done) {
         this.timeout(3000);
 
-        fs.ensureDirSync(TEMP_DIR);
-        process.chdir(TEMP_DIR);
+        fs.ensureDirSync(TEST_TEMP_DIR);
+        process.chdir(TEST_TEMP_DIR);
         newCommandInstance.init();
 
         setTimeout(done, 2000);
     })
 
     after(function(done) {
-        fs.removeSync(TEMP_DIR);
+        fs.removeSync(TEST_TEMP_DIR);
         done();
     })
 
-    describe('When the new command is used', function() {
+    describe('When the new command is used for the project type: Dot NET', function() {
 
-        it('should populate the location', function() {
-            TEMP_DIR.should.be.a.directory().and.not.empty;
+        it('should populate the directory', function() {
+            TEST_TEMP_DIR.should.be.a.directory().and.not.empty;
+        })
+
+        it('should not include the `views` directory', function() {
+            var pathToTest = path.join(TEST_TEMP_DIR, 'views');
+
+            pathToTest.should.not.be.a.path();
+        })
+
+        it('should not include the `release.js` gulp partial', function() {
+            var pathToTest = path.join(TEST_TEMP_DIR, 'gulpTasks', 'release.js');
+
+            pathToTest.should.not.be.a.path();
         })
 
     })
