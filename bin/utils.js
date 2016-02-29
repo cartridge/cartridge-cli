cdc1e0345c0516d21d5f0bbf485d7c6f11a7e08f
tawashley <trisashley@gmail.com>
2016-02-29 16:25:50 +0000

    docs: Add doc blockr comments for utils log function

--- bin/utils.js
+++ bin/utils.js
@@ -2,6 +2,11 @@ var log = require('loglevel');
 
 var utilsApi = {};
 
+/**
+ * Set the internal log level and return a log module instance
+ * @param  {Object} options Command line options e.g silent, verbose
+ * @return {Object}         Log module instance
+ */
 utilsApi.getLogInstance = function(options) {
 	if(options.silent) {
 		log.setLevel(log.levels.SILENT);
