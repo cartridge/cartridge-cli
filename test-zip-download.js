a5fda76baa3803fa978b89b4fbf5d9ecc6415997
tawashley <trisashley@gmail.com>
2016-03-01 15:18:20 +0000

    chore: accidentally committed test file

--- test-zip-download.js
+++
@@ -1,7 +0,0 @@
-var request = require('request');
-
-request('http://twitter.github.com/bootstrap/assets/bootstrap.zip')
-  .pipe(fs.createWriteStream('bootstrap.zip'))
-  .on('close', function () {
-    console.log('File written!');
-  });
