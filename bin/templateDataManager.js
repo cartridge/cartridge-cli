397795e2e8d076244bf2ed54f8c0f0774a39f46f
tawashley <trisashley@gmail.com>
2016-03-07 17:22:12 +0000

    feat: projectGeneratedDate date is now padded e.g 1/2/2016 would now be 01/02/2016

--- bin/templateDataManager.js
+++ bin/templateDataManager.js
@@ -32,14 +32,33 @@ templateDataApi.getData = function() {
  * @return {Object} Meta data object
  */
 function getTemplateMetaData() {
-	var date = new Date();
-
 	return {
 		projectNameFileName:  _baseData.projectName.toLowerCase().replace(/ /g,"-"),
-		projectGeneratedDate: [date.getDate(), date.getMonth() + 1, date.getFullYear()].join('/'),
+		projectGeneratedDate: getCurrentDateFormatted(),
 		currentVersion:       pkg.version
 	}
 
+}
+/**
+ * Get the current DD/MM/YYYY formatted date
+ * @return {String} Current date
+ */
+function getCurrentDateFormatted() {
+	var date = new Date();
+	var day = padDateNumber(date.getDate());
+	var month = padDateNumber(date.getMonth() + 1);
+	var year = padDateNumber(date.getFullYear());
+
+	return [day, month, year].join('/');
+}
+
+/**
+ * Pad a date number if necessary e.g. 9 would be 09
+ * @param  {Number} number The number to pad
+ * @return {Number}        Padded number
+ */
+function padDateNumber(number) {
+	return number <= 9 ? '0' + number : number;
 }
 
 module.exports = templateDataApi;
