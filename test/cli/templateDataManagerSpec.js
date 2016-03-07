b8012abfa1a50866ce0e45b3a364febdf3060879
tawashley <trisashley@gmail.com>
2016-03-07 15:31:44 +0000

    test: Add more test for the template data manager

--- test/cli/templateDataManagerSpec.js
+++ test/cli/templateDataManagerSpec.js
@@ -3,6 +3,9 @@ var expect = chai.expect;
 
 chai.should();
 
+var path = require('path');
+var pkg = require(path.resolve(__dirname, '..', '..', 'package.json'));
+
 describe('As a user of the template data manager module', function() {
 	var templateDataManagerModule;
 
@@ -17,11 +20,12 @@ describe('As a user of the template data manager module', function() {
 			var expected = {};
 
 			actual.should.eql(expected);
+			actual.should.be.empty;
 		})
 
 	})
 
-	describe('When the project name file name contains a space', function() {
+	describe('When the project name contains a space', function() {
 
 		beforeEach(function() {
 			templateDataManagerModule.setData({
@@ -29,7 +33,7 @@ describe('As a user of the template data manager module', function() {
 			})
 		})
 
-		it('should correctly replace the space with a dash', function() {
+		it('should correctly replace the space with a dash for the projectNameFileName', function() {
 			var actual = templateDataManagerModule.getData().projectNameFileName;
 			var expected = 's3-plan';
 
@@ -39,7 +43,7 @@ describe('As a user of the template data manager module', function() {
 
 	})
 
-	describe('When the project name file is generated', function() {
+	describe('When the projectNameFileName is generated', function() {
 
 		beforeEach(function() {
 			templateDataManagerModule.setData({
@@ -57,4 +61,50 @@ describe('As a user of the template data manager module', function() {
 
 	})
 
+	describe('When the template data is generated', function() {
+
+		var templateData;
+
+		beforeEach(function() {
+			templateDataManagerModule.setData({
+				projectName: 's3 plan'
+			})
+
+			templateData = templateDataManagerModule.getData();
+		})
+
+		it('should have a projectNameFileName key', function() {
+			var actual = templateData.projectNameFileName;
+
+			expect(actual).to.exist;
+		})
+
+		it('should have a projectGeneratedDate key', function() {
+			var actual = templateData.projectGeneratedDate;
+
+			expect(actual).to.exist;
+		})
+
+		it('should have a currentVersion key', function() {
+			var actual = templateData.currentVersion;
+
+			expect(actual).to.exist;
+		})
+
+		it('should have the correct projectGeneratedDate value', function() {
+			var date = new Date();
+			var actual = templateData.projectGeneratedDate;
+			var expected = [date.getDate(), date.getMonth() + 1, date.getFullYear()].join('/');
+
+			actual.should.equal(expected);
+		})
+
+		it('should have the correct currentVersion value', function() {
+			var actual = templateData.currentVersion;
+			var expected = pkg.version;
+
+			actual.should.equal(expected);
+		})
+
+	})
 })
