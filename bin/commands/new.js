3ed33e35d622794e77879c262dcba7a6f4667b44
tawashley <trisashley@gmail.com>
2016-03-14 12:07:55 +0000

    refactor: move newCommand functions outside of module.export. Now export api object

--- bin/commands/new.js
+++ bin/commands/new.js
@@ -21,189 +21,186 @@ var _options;
 var CURRENT_WORKING_DIR = process.cwd();
 var TEMPLATE_FILES_PATH = path.join(CURRENT_WORKING_DIR, '_cartridge');
 
-module.exports = function(appDir) {
+var newCommandApi = {};
 
-	return {
-		init: init
-	};
+newCommandApi.init = function(options) {
+	_options = options;
+	_log = utils.getLogInstance(_options);
 
-	function init(options) {
-		_options = options;
-		_log = utils.getLogInstance(_options);
+	preSetup();
+	setupOnScreenPrompts();
+}
 
-		preSetup();
-		setupOnScreenPrompts();
-	}
+function preSetup() {
+	promptOptions.setup(_options);
 
-	function preSetup() {
-		promptOptions.setup(_options);
+	_log.warn('');
+	_log.warn(chalk.bold('Running through setup for a new project.'));
+	_log.warn(chalk.bold('This can be exited out by pressing [Ctrl+C]'));
+	_log.warn('');
 
-		_log.warn('');
-		_log.warn(chalk.bold('Running through setup for a new project.'));
-		_log.warn(chalk.bold('This can be exited out by pressing [Ctrl+C]'));
-		_log.warn('');
+	_log.warn(chalk.bold('Make sure you are running this command in the folder you want all files copied to'));
+}
 
-		_log.warn(chalk.bold('Make sure you are running this command in the folder you want all files copied to'));
-	}
+function setupOnScreenPrompts() {
+	promptOptions
+		.getNewCommandPromptOptions()
+	 	.then(function(promptOptions) {
+	 		console.log('');
+	 		inquirer.prompt(promptOptions, promptCallback);
+ 		})
+}
 
-	function setupOnScreenPrompts() {
-		promptOptions
-			.getNewCommandPromptOptions()
-		 	.then(function(promptOptions) {
-		 		console.log('');
-		 		inquirer.prompt(promptOptions, promptCallback);
-	 		})
-	}
+function promptCallback(answers) {
+	_promptAnswers = answers;
+	templateDataManager.setData(answers);
 
-	function promptCallback(answers) {
-		_promptAnswers = answers;
-		templateDataManager.setData(answers);
+	if(_promptAnswers.isOkToCopyFiles) {
 
-		if(_promptAnswers.isOkToCopyFiles) {
-
-			_log.info('');
-			_log.info('Inserting the cartridge...');
-
-			releaseService
-				.downloadLatestRelease(_options)
-				.then(copyCartridgeSourceFilesToCwd)
-
-		} else {
-			_log.info('User cancelled - no files copied')
-		}
-	}
-
-	function copyCartridgeSourceFilesToCwd(copyPath) {
-		fs.copy(copyPath, CURRENT_WORKING_DIR, {
-			filter: fileCopyFilter
-		}, fileCopyComplete)
-	}
-
-	function fileCopyFilter(path) {
-		var needToCopyFile = true;
-		var filesDirsToExclude = getCopyExcludeList();
-
-		for (var i = 0; i < filesDirsToExclude.length; i++) {
-			//Check if needToCopyFile is still true and
-			//hasn't been flipped during loop
-			if(needToCopyFile) {
-				needToCopyFile = path.indexOf(filesDirsToExclude[i]) === -1;
-			}
-		};
-
-		if(!needToCopyFile) {
-			_log.debug(chalk.underline('Skipping path - ' + path));
-		} else {
-			_log.debug('Copying path  -', path);
-		}
-
-		return needToCopyFile;
-	}
-
-	function fileCopyComplete(err) {
-		if (err) errorHandler(err);
-
-		templateCopiedFiles();
-	}
-
-	function getCopyExcludeList() {
-		//Default exclude folders / files
-		var excludeList = [
-			'node_modules'
-		];
-
-		if(_promptAnswers.projectType === "Dot NET") {
-			excludeList.push('views');
-			excludeList.push('release.js');
-		}
-
-		return excludeList;
-	}
-
-	function templateCopiedFiles() {
-		_log.debug('');
-		_log.info('Booting up files...');
-
-		fileTemplater.setConfig({
-			data: templateDataManager.getData(),
-			basePath: CURRENT_WORKING_DIR,
-			files: getTemplateFileList(),
-			onEachFile: singleFileCallback,
-			onCompleted: installNpmPackages
-		})
-
-		fileTemplater.run();
-	}
-
-	function getTemplateFileList() {
-		var fileList      = [];
-
-		// Creds file
-		fileList.push({
-			src:  path.join(TEMPLATE_FILES_PATH, 'creds.tpl'),
-			dest: path.join(CURRENT_WORKING_DIR, '_config', 'creds.json')
-		});
-
-		// Project package file
-		fileList.push({
-			src:  path.join(TEMPLATE_FILES_PATH, 'package.tpl'),
-			dest: path.join(CURRENT_WORKING_DIR, 'package.json')
-		});
-
-		// Project readme
-		fileList.push({
-			src:  path.join(TEMPLATE_FILES_PATH, 'readme.tpl'),
-			dest: path.join(CURRENT_WORKING_DIR, 'readme.md')
-		});
-
-		// Cartridge config
-		fileList.push({
-			src:  path.join(TEMPLATE_FILES_PATH, 'rc.tpl'),
-			dest: path.join(CURRENT_WORKING_DIR, '.cartridgerc')
-		});
-
-		return fileList;
-	}
-
-	function singleFileCallback(templateFilePath) {
-		_log.debug('Templating file -', templateFilePath);
-	}
-
-	function installNpmPackages() {
-		if(_promptAnswers.cartridgeModules.length > 0) {
-			console.log('');
-			_log.info('Installing expansion packs...');
-
-			npmInstallPackage(_promptAnswers.cartridgeModules, { saveDev: true}, function(err) {
-				if (err) errorHandler(err);
-
-				postInstallCleanUp();
-			})
-		} else {
-			postInstallCleanUp();
-		}
-	}
-
-	function postInstallCleanUp() {
-		_log.debug('');
-		_log.debug('Running post install cleanup');
-
-		releaseService.deleteReleaseTmpDirectory();
-
-		_log.debug('Deleting templates file directory: ' + TEMPLATE_FILES_PATH);
-
-		finishSetup();
-	}
-
-	function finishSetup() {
 		_log.info('');
-		_log.info(chalk.green('Setup complete!'));
-		_log.info('Cartridge project ' + chalk.yellow(_promptAnswers.projectName) + ' has been installed!');
-		_log.info('');
-		_log.info('Final steps:');
-		_log.info(' · Run ' + chalk.yellow('npm install') + ' to download all project dependencies. (If this fails you may need to run ' + chalk.yellow('sudo npm install') + ')');
-		_log.info(' · Run ' + chalk.yellow('gulp') + ' for initial setup of styles and scripts.');
-		_log.info(' · Run ' + chalk.yellow('gulp watch') + ' to setup watching of files.');
-		_log.info('');
+		_log.info('Inserting the cartridge...');
+
+		releaseService
+			.downloadLatestRelease(_options)
+			.then(copyCartridgeSourceFilesToCwd)
+
+	} else {
+		_log.info('User cancelled - no files copied')
 	}
 }
+
+function copyCartridgeSourceFilesToCwd(copyPath) {
+	fs.copy(copyPath, CURRENT_WORKING_DIR, {
+		filter: fileCopyFilter
+	}, fileCopyComplete)
+}
+
+function fileCopyFilter(path) {
+	var needToCopyFile = true;
+	var filesDirsToExclude = getCopyExcludeList();
+
+	for (var i = 0; i < filesDirsToExclude.length; i++) {
+		//Check if needToCopyFile is still true and
+		//hasn't been flipped during loop
+		if(needToCopyFile) {
+			needToCopyFile = path.indexOf(filesDirsToExclude[i]) === -1;
+		}
+	};
+
+	if(!needToCopyFile) {
+		_log.debug(chalk.underline('Skipping path - ' + path));
+	} else {
+		_log.debug('Copying path  -', path);
+	}
+
+	return needToCopyFile;
+}
+
+function fileCopyComplete(err) {
+	if (err) errorHandler(err);
+
+	templateCopiedFiles();
+}
+
+function getCopyExcludeList() {
+	//Default exclude folders / files
+	var excludeList = [
+		'node_modules'
+	];
+
+	if(_promptAnswers.projectType === "Dot NET") {
+		excludeList.push('views');
+		excludeList.push('release.js');
+	}
+
+	return excludeList;
+}
+
+function templateCopiedFiles() {
+	_log.debug('');
+	_log.info('Booting up files...');
+
+	fileTemplater.setConfig({
+		data: templateDataManager.getData(),
+		basePath: CURRENT_WORKING_DIR,
+		files: getTemplateFileList(),
+		onEachFile: singleFileCallback,
+		onCompleted: installNpmPackages
+	})
+
+	fileTemplater.run();
+}
+
+function getTemplateFileList() {
+	var fileList      = [];
+
+	// Creds file
+	fileList.push({
+		src:  path.join(TEMPLATE_FILES_PATH, 'creds.tpl'),
+		dest: path.join(CURRENT_WORKING_DIR, '_config', 'creds.json')
+	});
+
+	// Project package file
+	fileList.push({
+		src:  path.join(TEMPLATE_FILES_PATH, 'package.tpl'),
+		dest: path.join(CURRENT_WORKING_DIR, 'package.json')
+	});
+
+	// Project readme
+	fileList.push({
+		src:  path.join(TEMPLATE_FILES_PATH, 'readme.tpl'),
+		dest: path.join(CURRENT_WORKING_DIR, 'readme.md')
+	});
+
+	// Cartridge config
+	fileList.push({
+		src:  path.join(TEMPLATE_FILES_PATH, 'rc.tpl'),
+		dest: path.join(CURRENT_WORKING_DIR, '.cartridgerc')
+	});
+
+	return fileList;
+}
+
+function singleFileCallback(templateFilePath) {
+	_log.debug('Templating file -', templateFilePath);
+}
+
+function installNpmPackages() {
+	if(_promptAnswers.cartridgeModules.length > 0) {
+		console.log('');
+		_log.info('Installing expansion packs...');
+
+		npmInstallPackage(_promptAnswers.cartridgeModules, { saveDev: true}, function(err) {
+			if (err) errorHandler(err);
+
+			postInstallCleanUp();
+		})
+	} else {
+		postInstallCleanUp();
+	}
+}
+
+function postInstallCleanUp() {
+	_log.debug('');
+	_log.debug('Running post install cleanup');
+
+	releaseService.deleteReleaseTmpDirectory();
+
+	_log.debug('Deleting templates file directory: ' + TEMPLATE_FILES_PATH);
+
+	finishSetup();
+}
+
+function finishSetup() {
+	_log.info('');
+	_log.info(chalk.green('Setup complete!'));
+	_log.info('Cartridge project ' + chalk.yellow(_promptAnswers.projectName) + ' has been installed!');
+	_log.info('');
+	_log.info('Final steps:');
+	_log.info(' · Run ' + chalk.yellow('npm install') + ' to download all project dependencies. (If this fails you may need to run ' + chalk.yellow('sudo npm install') + ')');
+	_log.info(' · Run ' + chalk.yellow('gulp') + ' for initial setup of styles and scripts.');
+	_log.info(' · Run ' + chalk.yellow('gulp watch') + ' to setup watching of files.');
+	_log.info('');
+}
+
+module.exports = newCommandApi;
--- bin/programBuilder.js
+++ bin/programBuilder.js
@@ -3,9 +3,8 @@
 var program = require('commander');
 var path    = require('path');
 var os = require('os');
-var appDir  = path.resolve(os.tmpDir(), 'cartridge');
 
-var newCommand = require('./commands/new')(appDir);
+var newCommand = require('./commands/new');
 var pkg        = require(path.resolve(__dirname, '..', 'package.json'));
 
 /**
