63d78624425bbc8803475558ad82ee4b16f3d9ba
tawashley <trisashley@gmail.com>
2016-08-31 15:06:07 +0100

    feat: Add into fileTemplater ability to delete src file after templating has been completed

--- bin/fileTemplater.js
+++ bin/fileTemplater.js
@@ -36,22 +36,26 @@ fileTemplaterApi.setConfig = function(config) {
 /**
  * Template a single file.
  * Get the contents of a file, template it and re-write to the same file
- * @param  {Object} filePaths File paths data, containing file src and dest
+ * @param  {Object} templateFileConfig Config data, containing template file config
  */
-function templateFile(filePaths) {
+function templateFile(templateFileConfig) {
     var compiled;
     var output;
 
-    fs.readFile(filePaths.src, 'utf8', function(err, fileContents) {
+    fs.readFile(templateFileConfig.src, 'utf8', function(err, fileContents) {
         if (err) errorHandler(err);
 
         compiled = template(fileContents);
         output = compiled(_config.data);
 
-        fs.writeFile(filePaths.dest, output, 'utf8', function(err) {
+        fs.writeFile(templateFileConfig.dest, output, 'utf8', function(err) {
             if (err) errorHandler(err);
 
-            _config.onEachFile(filePaths.dest);
+            _config.onEachFile(templateFileConfig.dest);
+
+            if(templateFileConfig.deleteSrcFile) {
+              fs.unlinkSync(templateFileConfig.src);
+            }
 
             if(_fileNumber === _config.files.length) {
                 _config = _defaultConfig;
